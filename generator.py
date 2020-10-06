d17d5566a82ba8c5bc71113e28ad69582a438361
SreeGaneshTN <tnsreeganesh@gmail.com>
2020-10-06 22:01:43 +0530

    Script for image generator

--- generator.py
+++ generator.py
@@ -1,20 +1,31 @@
 import numpy as np
 import random
-from PIL import Image
-sample_size=10
+import matplotlib.pyplot as plt
+sample_size=100
+import os
 
-path='/.../CGAN/data/vertical'
+path1='/home/sreegs/Documents/CGAN/data/vertical/'
+path2='/home/sreegs/Documents/CGAN/data/horizontal/'
 
 for k in range(sample_size):
-    arr=np.array((8,8),dtype=float)
+    arr=np.zeros((8,8),dtype=float)
     for i in range(8):
         for j in range(8):
-            if i <=4:
+            if j < 4:
                 arr[i,j]=random.randrange(0,128)
             else:
                 arr[i,j]=256
-    im=Image.fromarray(arr)
-    im.save(path+'Vertical_'+str(k),format='jpeg')
+    plt.imshow(arr)
+    plt.imsave(path1+'vertical_'+str(k)+'.jpg',arr)
 
-
+for k in range(sample_size):
+    arr=np.zeros((8,8),dtype=float)
+    for i in range(8):
+        for j in range(8):
+            if i < 4:
+                arr[i,j]=random.randrange(0,128)
+            else:
+                arr[i,j]=256
+    plt.imshow(arr)
+    plt.imsave(path2+'horizontal_'+str(k)+'.jpg',arr)
             
